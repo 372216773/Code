6d0dcea47fd9a42c41984d1d1d22e688510c23b3
inviting <53403310+372216773@users.noreply.github.com>
2021-05-10 22:20:25 +0800

    Tree

--- 数据结构/线性表/src/Main.java
+++ 数据结构/线性表/src/Main.java
@@ -1,17 +1,39 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
-        int length = str.length();
-        int num = Integer.parseInt(str);
-        int result = 0;
-        while ((length--)>0) {
-            int num1 = ((num / (int) Math.pow(10, length)) % 2 == 0) ? 0 : 1;
-            result = result * 10 + num1;
-            num = num %(int) Math.pow(10,length);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println(method1(n,m)+method3(n,m));;
+
+    }
+
+    //辗转相除法求最大公约数
+    public static int method1(int num1, int num2) {
+        int temp = 0;
+        while (num2 != 0) {
+            temp = num1 % num2;
+            num1 = num2;
+            num2 = temp;
         }
-        System.out.println(result);
+        return num1;
+    }
+
+    //求最大公约数
+    public static int method2(int num1, int num2) {
+        int min = Math.min(num1, num2);
+        for (int i = min; i > 1; i--) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    //两数之积 = 最小公倍数*最大公约数
+    public static long method3(int num1, int num2) {
+        return (long)num1 * num2 / method1(num1, num2);
     }
 }
