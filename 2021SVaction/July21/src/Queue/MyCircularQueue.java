ff08bcfff26b94afd848be2b23c662e7d119fe0b
inviting <53403310+372216773@users.noreply.github.com>
2021-07-26 20:06:42 +0800

    数组模拟循环队列

---
+++ 2021SVaction/July21/src/Queue/MyCircularQueue.java
@@ -0,0 +1,80 @@
+package Queue;
+
+/**
+ * MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
+ * circularQueue.enQueue(1);  // 返回 true
+ * circularQueue.enQueue(2);  // 返回 true
+ * circularQueue.enQueue(3);  // 返回 true
+ * circularQueue.enQueue(4);  // 返回 false，队列已满
+ * circularQueue.Rear();  // 返回 3
+ * circularQueue.isFull();  // 返 true回
+ * circularQueue.deQueue();  // 返回 true
+ * circularQueue.enQueue(4);  // 返回 true
+ * circularQueue.Rear();  // 返回 4
+ */
+class MyCircularQueue {
+
+    int[] elem;
+    int front;
+    int rear;
+
+    public MyCircularQueue(int k) {
+        this.elem = new int[k];
+    }
+
+    /**
+     * 进栈
+     * @param value 值
+     * @return 是否成功进栈
+     */
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        this.elem[rear] = value;
+        this.rear = (this.rear + 1) % this.elem.length;
+        return true;
+    }
+
+    /**
+     * 出栈
+     * @return
+     */
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        this.front = (this.front + 1) % this.elem.length;
+        return true;
+    }
+
+    /**
+     * 队首
+     * @return
+     */
+    public int Front() {
+        if (isEmpty()) return -1;
+            return this.elem[this.front];
+    }
+
+    /**
+     * 队尾
+     * @return
+     */
+    public int Rear() {
+        if (isEmpty()) return -1;
+        return this.elem[(this.rear +this.elem.length - 1) % this.elem.length];
+    }
+
+    /**
+     * 判空
+     * @return
+     */
+    public boolean isEmpty() {
+        return this.front == this.rear;
+    }
+
+    /**
+     * 判满
+     * @return
+     */
+    public boolean isFull() {
+        return (this.rear + 1) % this.elem.length == this.front;
+    }
+}
