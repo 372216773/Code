296252e3c16ce36cec1b48804d2aecf79d0f4d6d
inviting <53403310+372216773@users.noreply.github.com>
2021-08-15 17:35:14 +0800

    解析xml文件

--- 2021SVaction/xml/src/Test/xmlParse.java
+++ 2021SVaction/xml/src/Test/xmlParse.java
@@ -1,23 +1,30 @@
 package Test;
 
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SaxParse<T> {
+public class xmlParse<T> {
     DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
     DocumentBuilder documentBuilder;
     Document document = null;
     List<T> list = new ArrayList<>();
     Person person;
 
-    public List<T> parse(String pathname) {
+    public List<T> domParse(String pathname) {
         try {
             documentBuilder = documentBuilderFactory.newDocumentBuilder();
             document = documentBuilder.parse(pathname);
@@ -55,4 +62,5 @@ public class SaxParse<T> {
         }
         return list;
     }
+
 }
