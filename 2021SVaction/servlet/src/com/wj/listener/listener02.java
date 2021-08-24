567cf7f4beb60bca57998494662ee7182fbad1a7
wangjing <372216773@qq.com>
2021-08-24 16:32:45 +0800

    servlet

--- 2021SVaction/servlet/src/com/wj/listener/listener02.java
+++ 2021SVaction/servlet/src/com/wj/listener/listener02.java
@@ -1,4 +1,29 @@
 package com.wj.listener;
 
-public class listener02 {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class listener02 implements ServletContextListener, HttpSessionListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("监听到servletContext初始化init......");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("监听到servletContext销毁......");
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        System.out.println("");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        System.out.println("");
+    }
 }
