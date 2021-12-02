312d53491ca309f2ac2f0280f3796e2c34820180
wangjing <372216773@qq.com>
2021-12-02 16:49:48 +0800

    update

---
+++ 2021SVaction/spring-web/src/main/java/com/wj/springweb/service/UserService.java
@@ -0,0 +1,14 @@
+package com.wj.springweb.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    //private UserDao userDao;
+
+    public void add() {
+        System.out.println("userService add");
+    }
+}
